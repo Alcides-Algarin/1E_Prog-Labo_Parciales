9829af3ada26b718999bc638b4250e917d48ad53
Alcides Algarin <algarinalcides@gmail.com>
2022-05-19 18:00:27 -0300

    First commit

---
+++ Laboratorio_1_PrimerParcial/src/Laboratorio_1_PrimerParcial.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Laboratorio_1_PrimerParcial.c
+ Author      : Alcides Algarin
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Voy directo a recu."); /* prints Voy directo a recu. */
+	return EXIT_SUCCESS;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# 1E_Prog-Labo_Parciales
